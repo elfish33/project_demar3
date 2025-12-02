1b3bbb1507650c437cba75f330d136916fc1523a
폴리나 <elfish33@gmail.com>
2025-12-02 16:03:29 +0900

    Responsive Finished

--- script/index.js
+++ script/index.js
@@ -14,7 +14,7 @@ const mainSwiper = new Swiper ('#main_swiper', {
 const newPrdSwiper = new Swiper ('#swiper_new_prd',{
     loop: false,
     autoplay: {delay: 3000},
-    slidesPerView: 1,
+    slidesPerView: 2,
     spaceBetween: 0,
     navigation: {
         prevEl: '#swiper_new_prd .swiper-button-prev',
@@ -35,48 +35,68 @@ const newPrdSwiper = new Swiper ('#swiper_new_prd',{
 const EssentialSwiper = new Swiper ('#essential_swiper',{
     loop: true,
     autoplay: {delay: 3000},
-    slidesPerView: 1,
+    slidesPerView: 1.6,
     spaceBetween: 5,
     scrollbar: {
         el: '#essential_swiper .swiper-scrollbar',
     },
     breakpoints: {
         768: {
-            slidesPerView: 1.2,
+            slidesPerView: 2.4,
+            spaceBetween: 10,
+        },
+        890: {
+            slidesPerView: 3,
             spaceBetween: 10,
         },
         1024: {
-            slidesPerView: 2,
+            slidesPerView: 3.2,
             spaceBetween: 15,
         },
         1300: {
-            slidesPerView: 3,
+            slidesPerView: 3.6,
             spaceBetween: 20,
         },
     },
 })
 
 const reviewSwiper = new Swiper('#review_swiper',{
-    slidesPerView: 1,
+    slidesPerView: 1.3,
+    spaceBetween: 5,
     loop: true,
-    /* autoplay: {delay:3000}, */
+    //autoplay: {delay:3000},
     centeredSlides: true,
-    navigation: {
-        prevEl: '#review_swiper .swiper-button-prev',
-        nextEl: '#review_swiper .swiper-button-next',
-    },
     breakpoints: {
         768: {
-            slidesPerView: 2.5,
+            slidesPerView: 2,
             spaceBetween: 10,
         },
-        1024: {
+        918: {
+            slidesPerView: 2.6,
+            spaceBetween: 10,
+        },
+        1038: {
+            slidesPerView: 2.8,
+            spaceBetween: 10,
+        },
+        1230: {
             slidesPerView: 3.5,
             spaceBetween: 10,
         },
         1300: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+        },
+        1435: {//!!!
             slidesPerView: 4.5,
             spaceBetween: 10,
         },
     },
+})
+
+const searchResetBtn = document.querySelector('#search_reset_btn')
+const menuSearchInput = document.querySelector('#menu_search_input')
+
+searchResetBtn.addEventListener('click', ()=>{
+    menuSearchInput.value = '';
 })
